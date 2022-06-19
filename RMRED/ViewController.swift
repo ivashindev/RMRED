d70a89e5f90c5ddc45fdf393d9d948496deffb76
Dima <ivashindev@gmail.com>
2022-06-19 18:22:34 -0400

    Initial Commit

---
+++ RMRED/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RMRED
+//
+//  Created by Dmitry Ivashin on 19.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
