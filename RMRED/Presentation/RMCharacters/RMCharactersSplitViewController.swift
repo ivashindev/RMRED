2a2848840dbe8c5b3008edf818aa087f4c665bc6
Dima <ivashindev@gmail.com>
2022-06-21 07:55:28 -0400

    Add iPhone Master-first presentation and gitignore

---
+++ RMRED/Presentation/RMCharacters/RMCharactersSplitViewController.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class RMCharactersSplitViewController: UISplitViewController,
+                                  UISplitViewControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.delegate = self
+        self.preferredDisplayMode = UISplitViewController.DisplayMode.oneBesideSecondary
+    }
+    
+    func splitViewController(_ svc: UISplitViewController, topColumnForCollapsingToProposedTopColumn proposedTopColumn: UISplitViewController.Column) -> UISplitViewController.Column {
+        return .primary
+    }
+}
